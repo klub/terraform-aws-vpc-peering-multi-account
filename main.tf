ff2b2f8ef8e9b194f19c477cda03ff071fd79758
Bonamy Klu <bonamyklu@gmail.com>
2019-11-13 18:08:06 +0000

    test enable

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ locals {
 //  enabled = "${var.enabled == "true"}"
 //  enabled = "${var.enabled == "true" ? "true" : "false"}"
   enabled = "false"
-  count   = "${local.enabled == "true" ? 1 : 0}"
+//  count   = "${local.enabled == "true" ? 1 : 0}"
+  count   = 0
 }
